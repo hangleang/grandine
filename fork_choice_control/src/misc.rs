6f9db55dcb318e8e7e939ee7af5239990ba6d423
Mdominykas <dominykas@grandine.io>
2024-04-13 22:48:10 +0300

    Implemented PeerDAS helper_functions
    * implemented get_custody_columns
    
    * implemented compute_extended_matrix
    
    * implemented compute_extended_matrix
    
    * implemented recover_matrix
    
    * implemented get_data_column_sidecars
    
    * moved c-kzg to dependencies
    
    * further validation
    
    * moved data sidecar verification to fork_choice_store
    
    * data sidecar verifcation
    
    * fixed bug
    
    * added tests. They fail, but in a same way as lighthouse
    
    * added part of networking
    
    * make it compile
    
    * use consensus spec tests latest versions

--- fork_choice_control/src/misc.rs
+++ fork_choice_control/src/misc.rs
@@ -16,6 +16,7 @@ use types::{
         containers::{BlobIdentifier, BlobSidecar},
         primitives::BlobIndex,
     },
+    eip7594::DataColumnIdentifier,
     phase0::primitives::ValidatorIndex,
     preset::Preset,
 };
@@ -124,6 +125,10 @@ pub enum MutatorRejectionReason {
     #[strum(serialize = "invalid_blob_sidecar")]
     InvalidBlobSidecar {
         blob_identifier: BlobIdentifier,
+    },
+    #[strum(serialize = "invalid_data_column_sidecar")]
+    InvalidDataColumnSidecar {
+        data_column_identifier: DataColumnIdentifier,
     },
 }
 
