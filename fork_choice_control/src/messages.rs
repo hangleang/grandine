b9be1d85b7d003c12ffc274f32a3a24a849a5471
Tumas Bajoras <tumas.bajoras@gmail.com>
2024-05-12 14:46:38 +0300

    Add DataColumnsByRoot rpc
    * Handle DataColumnSidecars received from RPC by root
    
    * Request needed data columns from mutator
    
    * remove todo! macros in network
    
    * Removed todo macro from fork choice mutator
    
    * Handle DataColumnsByRoot RPC request (no storage)

--- fork_choice_control/src/messages.rs
+++ fork_choice_control/src/messages.rs
@@ -15,7 +15,7 @@ use execution_engine::PayloadStatusV1;
 use fork_choice_store::{
     AggregateAndProofOrigin, AttestationAction, AttestationItem, AttestationValidationError,
     AttesterSlashingOrigin, BlobSidecarAction, BlobSidecarOrigin, BlockAction, BlockOrigin,
-    ChainLink, DataColumnSidecarAction, DataColumnSidecarOrigin, Store,
+    ChainLink, DataColumnSidecarAction, DataColumnSidecarOrigin,
 };
 use log::debug;
 use serde::Serialize;
@@ -182,6 +182,7 @@ pub enum P2pMessage<P: Preset> {
     PublishBlobSidecar(Arc<BlobSidecar<P>>),
     Reject(Option<GossipId>, MutatorRejectionReason),
     BlockNeeded(H256, Option<PeerId>),
+    DataColumnsNeeded(Vec<DataColumnIdentifier>, Slot, Option<PeerId>),
     FinalizedCheckpoint(Checkpoint),
     HeadState(#[cfg_attr(test, derivative(Debug = "ignore"))] Arc<BeaconState<P>>),
 }
