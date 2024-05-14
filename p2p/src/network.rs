8c954d34a73fb37df44bddd951caad51dc757ad6
Tumas <tumas@grandine.io>
2024-05-14 14:21:47 +0300

    Handle DataColumnsByRange request

--- p2p/src/network.rs
+++ p2p/src/network.rs
@@ -1219,8 +1219,76 @@ impl<P: Preset> Network<P> {
         request_id: IncomingRequestId,
         request: DataColumnsByRangeRequest,
     ) -> Result<()> {
-        // TODO(feature/eip7549): implement this
         debug!("received DataColumnsByRange request (peer_id: {peer_id}, request: {request:?})");
+
+        let DataColumnsByRangeRequest {
+            start_slot,
+            count,
+            columns,
+        } = request;
+
+        let controller = self.controller.clone_arc();
+
+        // TODO(feature/eip7594): MIN_EPOCHS_FOR_DATA_COLUMN_SIDECARS_REQUESTS
+        // Let data_column_serve_range be
+        //  [max(current_epoch - MIN_EPOCHS_FOR_DATA_COLUMN_SIDECARS_REQUESTS, EIP7594_FORK_EPOCH), current_epoch].
+        let start_slot = start_slot.max(misc::compute_start_slot_at_epoch::<P>(
+            self.controller.chain_config().eip7594_fork_epoch,
+        ));
+
+        // > Clients MAY limit the number of blocks and sidecars in the response.
+        let difference = count.min(
+            self.controller
+                .chain_config()
+                .max_request_data_column_sidecars,
+        );
+
+        let current_slot = self.controller.head_slot();
+        let end_slot = start_slot
+            .checked_add(difference)
+            .ok_or(Error::EndSlotOverflow {
+                start_slot,
+                difference,
+            })?
+            .min(current_slot);
+
+        let network_to_service_tx = self.network_to_service_tx.clone();
+
+        self.dedicated_executor
+            .spawn(async move {
+                let mut data_column_sidecars = controller.data_column_sidecars_by_range(start_slot..end_slot, &columns)?;
+
+                // The following data column sidecars, where they exist, MUST be sent in (slot, column_index) order.
+                data_column_sidecars.sort_by_key(|sidecar| (sidecar.slot(), sidecar.index));
+
+                for data_column_sidecar in data_column_sidecars {
+                    debug!(
+                        "sending DataColumnsSidecarsByRange response chunk \
+                        (peer_request_id: {peer_request_id:?}, peer_id: {peer_id}, data_column_sidecar: {data_column_sidecar:?})",
+                    );
+
+                    ServiceInboundMessage::SendResponse(
+                        peer_id,
+                        peer_request_id,
+                        request_id,
+                        Box::new(Response::DataColumnsByRange(Some(data_column_sidecar))),
+                    )
+                    .send(&network_to_service_tx);
+                }
+
+                debug!("terminating BlobSidecarsByRange response stream");
+
+                ServiceInboundMessage::SendResponse(
+                    peer_id,
+                    peer_request_id,
+                    request_id,
+                    Box::new(Response::DataColumnsByRange(None)),
+                )
+                .send(&network_to_service_tx);
+
+                Ok::<_, anyhow::Error>(())
+            })
+            .detach();
 
         Ok(())
     }
@@ -1475,7 +1543,31 @@ impl<P: Preset> Network<P> {
                     request_id: {request_id}",
                 );
             }
-            Response::DataColumnsByRange(Some(data_column_sidecar)) => {}
+            // TODO(feature/eip7594): This appears to be unfinished.
+            // > Before consuming the next response chunk, the response reader SHOULD verify the
+            // > data column sidecar is well-formatted, has valid inclusion proof, and is correct w.r.t. the expected KZG commitments
+            Response::DataColumnsByRange(Some(data_column_sidecar)) => {
+                debug!(
+                    "received DataColumnsByRange response chunk \
+                    (request_id: {request_id}, peer_id: {peer_id}, data_column_sidecar.slot: {:?})",
+                    data_column_sidecar.signed_block_header.message.slot,
+                );
+
+                let data_column_identifier = data_column_sidecar.as_ref().into();
+                let data_column_sidecar_slot = data_column_sidecar.signed_block_header.message.slot;
+
+                if self.register_new_received_data_column_sidecar(
+                    data_column_identifier,
+                    data_column_sidecar_slot,
+                ) {
+                    let block_seen = self
+                        .received_block_roots
+                        .contains_key(&data_column_identifier.block_root);
+
+                    P2pToSync::RequestedDataColumnSidecar(data_column_sidecar, block_seen, peer_id)
+                        .send(&self.channels.p2p_to_sync_tx);
+                }
+            }
             Response::DataColumnsByRange(None) => {
                 debug!(
                     "peer {peer_id} terminated DataColumnsByRange response stream for request_id: {request_id}",
